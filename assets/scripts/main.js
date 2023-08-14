7f5a4fab8c0dddea7fab30565a588507ef87e089
TonicComa37 <brett.david.wilson.bdw@outlook.com>
2023-08-14 12:24:19 +0200

    working buttons and logic.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,296 +1,3 @@
-// fetch('assets\\jsons\\humbrol.json')
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log(data[1]);
-//     // Perform further operations with the JSON data
-//   })
-//   .catch(error => {
-//     console.error('Error:', error);
-//   });
-
-
-// fetch('assets/jsons/humbrol.json')
-//   .then(response => response.json())
-//   .then(data => {
-//     const jsonData = data;
-//     const outputDiv = document.getElementById("itemOutput");
-
-//     console.log(jsonData);
-
-//     for (const key in jsonData) {
-//       const value = jsonData[key];
-//       const itemElement = document.createElement("p");
-//       itemElement.textContent = `${key} : ${value}`;
-//       outputDiv.appendChild(itemElement);
-//     }
-
-//     // Perform further operations with the JSON data
-//   })
-//   .catch(error => {
-//     console.error('Error:', error);
-//   });
-
-// var jsonDataMain
-
-// fetch('assets\\jsons\\humbrolPiants.json')
-// .then(response => response.json())
-// .then(data => {
-//   const jsonData = data;
-//   jsonDataMain = jsonData
-//   const outputDiv = document.getElementById("itemOutput");
-
-//   console.log(jsonData["1"]["name"]);
-
-//   for (const key in jsonData) {
-//     const value = jsonData[key];
-//     const itemElement = document.createElement("p");
-//     itemElement.textContent = `${key} : ${value}`;
-//     outputDiv.appendChild(itemElement);
-//   }
-
-//   // Perform further operations with the JSON data
-//   })
-//     .catch(error => {
-//     console.error('Error:', error);
-// });
-
-// console.log(jsonDataMain["1"]["name"]);
-
-// fetch('assets\\jsons\\humbrolPiants.json')
-// .then(response => response.json())
-// .then(data => {
-//   const jsonData = data;
-//   jsonDataMain = jsonData
-//   const outputDiv = document.getElementById("itemOutput");
-
-//   // console.log(jsonData["1"]["name"]);
-
-//   for (const key in jsonData) {
-//     const value = jsonData[key];
-//     const itemElement = document.createElement("p");
-//     itemElement.textContent = `${value["name"]} : ${value["id"]} : ${value["have"]}`;
-//     outputDiv.appendChild(itemElement);
-//   }
-  
-//   // Perform further operations with the JSON data
-//   })
-//     .catch(error => {
-//     console.error('Error:', error);
-// });
-
-
-
-// let jsonData;
-
-// function fetchData() {
-//   return fetch('assets\\jsons\\humbrolPiants.json')
-//     .then(response => response.json())
-//     .then(data => {
-//       jsonData = data;
-//       accessJsonData();
-//     })
-//     .catch(error => {
-//       console.error('Error:', error);
-//     });
-// }
-
-// function modifyJsonData() {
-//   const inputField = document.getElementById("inputField");
-//   const submitButton = document.getElementById("submitButton");
-//   // const outputDiv = document.getElementById("outputDiv");
-
-//   submitButton.addEventListener("click", function() {
-//     const inputValue = inputField.value;
-//     // outputDiv.textContent = inputValue;
-
-//     console.log(jsonData[inputValue]["have"] = true);
-//     console.log(jsonData); // Updated JSON data
-  
-//     // accessJsonData(); // Call accessJsonData() to redisplay the data
-//     displayInputData()
-//   });
-  
-// }
-
-// function displayInputData(){
-//   const outputDiv = document.getElementById("itemOutput");
-//   outputDiv.innerHTML = ""; // Clear previous display
-//   modifyJsonData();
-
-//   for (const key in jsonData) {
-//     const value = jsonData[key];
-//     if (jsonData[key]["have"] === true) {
-//       const itemElement = document.createElement("p");
-//       itemElement.textContent = `${value["id"]} : ${value["name"]} : have : ${value["have"]}`;
-//       outputDiv.appendChild(itemElement);
-//     }
-//   }
-// }
-
-// function displayAll(){
-//   const outputDiv = document.getElementById("itemOutputAll");
-//   outputDiv.innerHTML = ""; // Clear previous display
-//   modifyJsonData();
-
-//   for (const key in jsonData) {
-//     const value = jsonData[key];
-//     const itemElement = document.createElement("p");
-//     itemElement.textContent = `${value["id"]} : ${value["name"]} : have : ${value["have"]}`;
-//     outputDiv.appendChild(itemElement);
-//   }
-// }
-
-// function accessJsonData() {
-//   const outputDiv = document.getElementById("itemOutput");
-//   outputDiv.innerHTML = ""; // Clear previous display
-//   modifyJsonData();
-
-//   displayInputData()
-//   // displayAll()
-
-//   // for (const key in jsonData) {
-//   //   const value = jsonData[key];
-//   //   const itemElement = document.createElement("p");
-//   //   itemElement.textContent = `${value["id"]} : ${value["name"]} : have : ${value["have"]}`;
-//   //   outputDiv.appendChild(itemElement);
-//   // }
-
-// }
-
-// fetchData();
-
-
-// let jsonData;
-
-// function fetchData() {
-//   return fetch('assets\\jsons\\humbrolPiants.json')
-//     .then(response => response.json())
-//     .then(data => {
-//       jsonData = data; // Store the data in a variable
-//       // Call other functions that need to work with the data
-//       displayAll();
-//       displayUserData();
-      
-//       processData();
-//     })
-//     .catch(error => {
-//       console.error('Error:', error);
-//     });
-// }
-
-
-// function modifyJsonDataTrue() {
-//   const inputField = document.getElementById("inputField");
-//   const submitButton = document.getElementById("submitButton");
-//   // const outputDiv = document.getElementById("outputDiv");
-  
-//   submitButton.addEventListener("click", function() {
-//     const inputValue = inputField.value;
-//     // outputDiv.textContent = inputValue;
-
-//     for (const key in jsonData) {
-//       const cleanedName = jsonData[key].name.replace(/\s/g, "");
-//       const cleanedInputValue = inputValue.replace(/\s/g, "");
-//       if (cleanedName.toLowerCase() === cleanedInputValue.toLowerCase()) {
-//         jsonData[key].have = true;
-//         break;
-//       }
-//     }
-    
-//     try {
-
-//       // Code that might throw an error
-//       jsonData[inputValue]["have"] = true;
-//       throw new Error("Something went wrong BOB");
-//     } catch (error) {
-//       // Swallow the error and do nothing
-//     }
-//     // console.log(jsonData); // Updated JSON data
-    
-//     // accessJsonData(); // Call accessJsonData() to redisplay the data
-//     displayUserData()
-//     displayAll()
-//   });
-// }
-
-// function modifyJsonDataFalse() {
-//   const inputField = document.getElementById("inputFieldFalse");
-//   const submitButton = document.getElementById("submitButtonFalse");
-//   // const outputDiv = document.getElementById("outputDiv");
-  
-//   submitButton.addEventListener("click", function() {
-//     const inputValue = inputField.value;
-//     // outputDiv.textContent = inputValue;
-
-//     for (const key in jsonData) {
-//       const cleanedName = jsonData[key].name.replace(/\s/g, "");
-//       const cleanedInputValue = inputValue.replace(/\s/g, "");
-//       if (cleanedName.toLowerCase() === cleanedInputValue.toLowerCase()) {
-//         jsonData[key].have = true;
-//         break;
-//       }
-//     }
-    
-//     try {
-
-//       // Code that might throw an error
-//       jsonData[inputValue]["have"] = false;
-//       throw new Error("Something went wrong BOB");
-//     } catch (error) {
-//       // Swallow the error and do nothing
-//     }
-//     // console.log(jsonData); // Updated JSON data
-    
-//     // accessJsonData(); // Call accessJsonData() to redisplay the data
-//     displayUserData()
-//     displayAll()
-//   });
-// }
-
-// function displayAll(){
-//   const outputDiv = document.getElementById("itemOutputAll");
-//   outputDiv.innerHTML = ""; // Clear previous display
-//   modifyJsonDataTrue()
-//   modifyJsonDataFalse()
-  
-//   for (const key in jsonData) {
-//     const value = jsonData[key];
-//     const itemElement = document.createElement("p");
-//     if (value["have"] === true) {
-//       itemElement.innerHTML = `<br>${value["id"]} : ${value["name"]} : <span style="color: green;">HAVE</span>`;
-//     } else {
-//       itemElement.innerHTML = `<br>${value["id"]} : ${value["name"]} : <span style="color: red;">NEED</span>`;
-//     }    
-//     outputDiv.appendChild(itemElement);
-//   }
-// }
-
-// function displayUserData(){
-//   const outputDiv = document.getElementById("itemOutput");
-//   outputDiv.innerHTML = ""; // Clear previous display
-//   modifyJsonDataTrue()
-//   modifyJsonDataFalse()
-  
-//   for (const key in jsonData) {
-//     const value = jsonData[key];
-//     if (jsonData[key]["have"] === true) {
-//       const itemElement = document.createElement("p");
-//       itemElement.innerHTML = `<br>${value["id"]} : ${value["name"]}<br>`;
-//       outputDiv.appendChild(itemElement);      
-//     }
-//   }
-// }
-
-// function processData() {
-//   // Work with the data here
-//   modifyJsonDataTrue();
-//   modifyJsonDataFalse()
-//   console.log(jsonData);
-// }
-
-// fetchData();
-
-
 let jsonData;
 
 function fetchData() {
@@ -298,9 +5,9 @@ function fetchData() {
     .then(response => response.json())
     .then(data => {
       jsonData = data;
-      displayAll();
-      displayUserData();
-      displayUserDataModel();
+      displayAllPaints();
+      displayUserPaints();
+      displayModelPaints();
       processData();
     })
     .catch(error => {
@@ -308,12 +15,15 @@ function fetchData() {
     });
 }
 
-function modifyJsonDataTrue() {
-  const inputField = document.getElementById("inputField");
+function addPaintsToHave() {
+  const inputAddPaint = document.getElementById("inputAddPaint");
   const submitButton = document.getElementById("submitButton");
 
+
+  
+
   submitButton.addEventListener("click", function() {
-    const inputValue = inputField.value;
+    const inputValue = inputAddPaint.value;
 
     for (const key in jsonData) {
       const cleanedName = jsonData[key].name.replace(/\s/g, "");
@@ -331,19 +41,20 @@ function modifyJsonDataTrue() {
       // Swallow the error and do nothing
     }
 
-    displayUserData();
-    displayAll();
-    displayUserDataModel()
-    displayNeedOnly()
+    displayUserPaints();
+    displayAllPaints();
+    displayModelPaints()
+    displayNeededPaintsOnly()
   });
 }
 
-function modifyJsonDataFalse() {
-  const inputField = document.getElementById("inputFieldFalse");
+
+function removePiantsFromHave() {
+  const inputAddPaint = document.getElementById("inputRemovePaint");
   const submitButton = document.getElementById("submitButtonFalse");
 
   submitButton.addEventListener("click", function() {
-    const inputValue = inputField.value;
+    const inputValue = inputAddPaint.value;
 
     for (const key in jsonData) {
       const cleanedName = jsonData[key].name.replace(/\s/g, "");
@@ -361,12 +72,14 @@ function modifyJsonDataFalse() {
       // Swallow the error and do nothing
     }
 
-    displayUserData();
-    displayAll();
+    displayUserPaints();
+    displayAllPaints();
+    displayModelPaints()
+    displayNeededPaintsOnly()
   });
 }
 
-function displayAll() {
+function displayAllPaints() {
   const outputDiv = document.getElementById("itemOutputAll");
   outputDiv.innerHTML = "";
 
@@ -374,29 +87,29 @@ function displayAll() {
     const value = jsonData[key];
     const itemElement = document.createElement("p");
     if (value["have"] === true) {
-      itemElement.innerHTML = `<br>${value["id"]} : ${value["name"]} : <span style="color: green;">HAVE</span>`;
+      itemElement.innerHTML = `${value["id"]} : ${value["name"]} : <span style="color: green;">HAVE</span>`;
     } else {
-      itemElement.innerHTML = `<br>${value["id"]} : ${value["name"]} : <span style="color: red;">NEED</span>`;
+      itemElement.innerHTML = `${value["id"]} : ${value["name"]} : <span style="color: red;">NEED</span>`;
     }
     outputDiv.appendChild(itemElement);
   }
 }
 
-function displayUserData() {
-  const outputDiv = document.getElementById("itemOutput");
+function displayUserPaints() {
+  const outputDiv = document.getElementById("itemOutputUserPaints");
   outputDiv.innerHTML = "";
 
   for (const key in jsonData) {
     const value = jsonData[key];
     if (jsonData[key]["have"] === true) {
       const itemElement = document.createElement("p");
-      itemElement.innerHTML = `<br>${value["id"]} : ${value["name"]}<br>`;
+      itemElement.innerHTML = `${value["id"]} : ${value["name"]}<br>`;
       outputDiv.appendChild(itemElement);
     }
   }
 }
 
-function displayUserDataModel() {
+function displayModelPaints() {
   const outputDiv = document.getElementById("itemOutputModelsNeed");
   outputDiv.innerHTML = "";
 
@@ -408,30 +121,8 @@ function displayUserDataModel() {
       value["need"] = false
       value["get"] = false
     }
-    displayNeedOnly()
+    displayNeededPaintsOnly()
   });
-
-  // for (const key in jsonData) {
-  //   const value = jsonData[key];
-  //   if (jsonData[key]["need"] === true) {
-  //     const itemElement = document.createElement("p");
-  //     itemElement.innerHTML = `<br>${value["id"]} : ${value["name"]}<br>`;
-  //     outputDiv.appendChild(itemElement);
-  //   }
-  // }
-
-//   for (const key in jsonData) {
-//     const value = jsonData[key];
-//     const itemElement = document.createElement("p");
-//     if (jsonData[key]["have"] === true){
-//     if (value["have"] === value["need"]) {
-//       itemElement.innerHTML = `<br>${value["id"]} : ${value["name"]} : <span style="color: green;">HAVE</span>`;
-//     } else {
-//       itemElement.innerHTML = `<br>${value["id"]} : ${value["name"]} : <span style="color: red;">NEED</span>`;
-//     }
-//   }
-//   outputDiv.appendChild(itemElement);
-// }
 
     for (const key in jsonData) {
     const value = jsonData[key];
@@ -439,11 +130,11 @@ function displayUserDataModel() {
       const itemElement = document.createElement("p");
       // itemElement.innerHTML = `<br>${value["id"]} : ${value["name"]}<br>`;
       if (value["have"] === value["need"]) {
-        itemElement.innerHTML = `<br>${value["id"]} : ${value["name"]} : <span style="color: green;">HAVE</span>`;
+        itemElement.innerHTML = `${value["id"]} : ${value["name"]} : <span style="color: green;">HAVE</span>`;
         jsonData[key]["get"] = false
       }
       else{
-        itemElement.innerHTML = `<br>${value["id"]} : ${value["name"]} : <span style="color: red;">NEED</span>`;
+        itemElement.innerHTML = `${value["id"]} : ${value["name"]} : <span style="color: red;">NEED</span>`;
         jsonData[key]["get"] = true
       }
       outputDiv.appendChild(itemElement);
@@ -451,13 +142,13 @@ function displayUserDataModel() {
   }
 }
 
-function comparData() {
-  const inputField = document.getElementById("inputFieldModel");
+function comparPaintData() {
+  const inputAddPaint = document.getElementById("inputFieldModel");
   const submitButton = document.getElementById("submitButtonModel");  const removeButton = document.getElementById("submitButtonModel");
 
   submitButton.addEventListener("click", function() {
-    console.log("Button clicked");
-    const inputValue = inputField.value;
+    // console.log("Button clicked");
+    const inputValue = inputAddPaint.value;
 
     for (const key in jsonData) {
       const cleanedName = jsonData[key].name.replace(/\s/g, "");
@@ -475,18 +166,13 @@ function comparData() {
       // Swallow the error and do nothing
     }
 
-    displayUserDataModel();
-    // for (const key in jsonData){
-    //   if (jsonData[key]["have"] !== jsonData[key]["need"]) {
-    //     displayNeedOnly()
-    //   }
-    // }
-    displayNeedOnly()
-    console.log(jsonData);
+    displayModelPaints();
+    displayNeededPaintsOnly()
+    // console.log(jsonData);
   });
 }
 
-function displayNeedOnly() {
+function displayNeededPaintsOnly() {
   const outputDiv = document.getElementById("itemOutputModelsNeedOnly");
   outputDiv.innerHTML = "";
 
@@ -494,7 +180,7 @@ function displayNeedOnly() {
     const value = jsonData[key];
     if (jsonData[key]["get"] === true) {
       const itemElement = document.createElement("p");
-      itemElement.innerHTML = `<br>${value["id"]} : ${value["name"]} : <span style="color: red;">NEED</span>`;
+      itemElement.innerHTML = `${value["id"]} : ${value["name"]} : <span style="color: red;">NEED</span>`;
       outputDiv.appendChild(itemElement);
     }
   }
@@ -502,10 +188,10 @@ function displayNeedOnly() {
 
 
 function processData() {
-  modifyJsonDataTrue();
-  modifyJsonDataFalse();
-  comparData();
-  console.log(jsonData);
+  addPaintsToHave();
+  removePiantsFromHave();
+  comparPaintData();
+  // console.log(jsonData);
 }
 
 fetchData();
